162b931d6d31760aa25d45da19184a1a97393117
eindiran <eindiran@promptu.com>
2018-09-11 15:19:06 -0700

    Fix argparse descriptions and handle encoding errors more gracefully

--- orgmode_to_anki.py
+++ orgmode_to_anki.py
@@ -1,5 +1,5 @@
 """
-Read in an org-mode and convert it to a tab-separated csv file suitable for use with Anki.
+Read in an org-mode and convert it to a tab-separated file suitable for use with Anki.
 """
 from typing import List
 import argparse
@@ -57,19 +57,25 @@ def write_cards_to_file(cards: List[str], output_file: str, append: bool) -> Non
 def main() -> None:
     """Use read_orgmode_file() to read in a file, then operate on the resulting notes."""
     parser = argparse.ArgumentParser(description='Reads in an org-mode file and generates a ' +
-                                     'matching YAML file for dnote.')
+                                     'matching Anki card with HTML formatting.')
     parser.add_argument('-i', '--input', nargs=1, type=str, required=True, dest='input_file',
                         help='Location of the org-mode file to consume.')
     parser.add_argument('-o', '--output', nargs=1, type=str, required=True, dest='output_file',
-                        help='Output file where YAML notes are written.')
+                        help='Output file where HTML-formatted Anki cards are written.')
     parser.add_argument('-a', '--append', action='store_true', help='Append to output file, ' +
                         'rather than writing to it.')
     args = parser.parse_args()
+    # Argparse guarantees that we don't need to wrap the next two lines in a try-except
     input_file = args.input_file[0].strip()
     output_file = args.output_file[0].strip()
-    notes = read_orgmode_file(input_file)
-    cards = [format_card(note) for note in notes]
-    write_cards_to_file(cards, output_file, args.append)
+    try:
+        notes = read_orgmode_file(input_file)
+        cards = [format_card(note) for note in notes]
+        write_cards_to_file(cards, output_file, args.append)
+    except UnicodeDecodeError as err:
+        print('Text in the input file {} was not UTF-8 encoded.'.format(input_file), err)
+        return
+    print('Done! Anki cards written to "{}"'.format(output_file))
 
 
 if __name__ == '__main__':
